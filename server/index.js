2f5131c1371733537745af0db0ca81dbc5aee704
krishna-deo <krishnadeo11096@gmail.com>
2026-08-30 15:30:43 +0530

    fix: load dotenv file using absolute path in server/index.js to ensure environment variables are resolved when running from root

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
